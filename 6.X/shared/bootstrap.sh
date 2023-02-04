dd679c42addfa50382bf7e1b58ea6cd4cddc0dc8
Marius Luexmann <m.luexmann@gmail.com>
2023-02-04 16:10:22 +0100

    bootstrap.sh

--- 6.X/shared/bootstrap.sh
+++ 6.X/shared/bootstrap.sh
@@ -1,11 +1,20 @@
 #!/bin/sh
 
 # Import SSH config
+printf "\n\nStarting import of SSH config...\n\n"
+
 mkdir -p /tmp/.ssh/
 cp -R /tmp/.ssh/ /root/
 chown -R root:root /root/.ssh
 
-# Start php-fpm
-php-fpm --daemonize
+printf "\n\nDone.\n\n"
+
+# See docker-php-entrypoint
+set -e
+
+# first arg is `-f` or `--some-option`
+if [ "${1#-}" != "$1" ]; then
+	set -- php-fpm "$@"
+fi
 
 exec "$@"
